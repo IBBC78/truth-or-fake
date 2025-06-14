a08779bc59b8bdb95ab3c82bc395562d67a5a51d
IBBC78 <99761517+IBBC78@users.noreply.github.com>
2025-06-14 13:13:38 +0200

    Add HeaderTabs component with styling for improved layout and UI

---
+++ src/components/HeaderTabs.tsx
@@ -0,0 +1,23 @@
+import {
+  Container,  
+} from '@mantine/core';
+import { ActionToggle } from './colorActionToggle';
+import classes from '../css/HeaderTabs.module.css';
+
+
+export function HeaderTabs() {
+
+  return (
+    <div className={classes.header}>
+      <Container className={classes.mainSection} size="md" justify-contents="space-between" align-items="center">
+        <ActionToggle/>
+        <div className={classes.title}>
+          <h3>Truth or Fake</h3>
+        </div>
+        <div className={classes.subtitle}>
+          <p>made by Ibrahima</p>
+        </div>
+      </Container>
+    </div>
+  );
+}
